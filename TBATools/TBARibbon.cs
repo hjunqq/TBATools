d38fadcb5c2de99be940ae08e8d255c0e6090abf
hjunqq <qhjun@live.com>
2017-11-14 14:53:54 +0800

    Signed-off-by: hjunqq <qhjun@live.com>

---
+++ TBATools/TBARibbon.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace TBATools
+{
+    public partial class TBARibbon
+    {
+        private void TBARibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        public Excel.Range GetDataRange(string Prompt, object Default)
+        {
+            Excel.Range actRange = Globals.ThisAddIn.Application.ActiveWindow.RangeSelection;
+            try
+            {
+                actRange = Globals.ThisAddIn.Application.InputBox(
+                    Prompt,
+                    "TBATools",
+                    Default,
+                    System.Type.Missing,
+                    System.Type.Missing,
+                    System.Type.Missing,
+                    System.Type.Missing,
+                    8);
+            }
+            catch
+            {
+                actRange = null;
+            }
+            return actRange;
+        }
+
+        private void btnToColByCol_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range actRange = Globals.ThisAddIn.Application.ActiveWindow.RangeSelection;
+
+            actRange = GetDataRange("已选择范围：", actRange.Address.ToString());
+
+            if (actRange == null)
+            {
+                return;
+            }
+
+            Excel.Range resRange;
+
+            resRange = GetDataRange("选择结果地址", System.Type.Missing);
+
+            if (resRange == null)
+            {
+                return;
+            }
+            try
+            {
+                int nRow = actRange.Rows.Count;
+                int nCol = actRange.Columns.Count;
+                int iCount = 0;
+                for (int iCol = 1; iCol <= nCol; iCol++)
+                {
+                    for (int iRow = 1; iRow <= nRow; iRow++)
+                    {
+                        iCount++;
+                        resRange.Cells[iCount, 1] = actRange.Cells[iRow, iCol];
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            resRange.Select();
+        }
+
+        private void btnAbout_Click(object sender, RibbonControlEventArgs e)
+        {
+            Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorkSheet();
+            //currentSheet.Range["A1"].Value = "Hello World";
+            //currentSheet.Columns.AutoFit();
+            int iCount = 0;
+            for (int iRow = 1; iRow < 10; iRow++)
+            {
+                for (int iCol = 1; iCol < 3; iCol++)
+                {
+                    iCount++;
+                    currentSheet.Cells[iRow, iCol] = iCount;
+                }
+            }
+
+            string version = System.Reflection.Assembly.GetExecutingAssembly()
+            .GetName()
+            .Version
+            .ToString();
+            System.Windows.Forms.MessageBox.Show(version,"版本号");
+        }
+
+        private void btnToColByRow_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range actRange = Globals.ThisAddIn.Application.ActiveWindow.RangeSelection;
+
+            actRange = GetDataRange("已选择范围：", actRange.Address.ToString());
+
+            if (actRange == null)
+            {
+                return;
+            }
+
+            Excel.Range resRange;
+
+            resRange = GetDataRange("选择结果地址", System.Type.Missing);
+
+            if (resRange == null)
+            {
+                return;
+            }
+            int nRow = actRange.Rows.Count;
+            int nCol = actRange.Columns.Count;
+            int iCount = 0;
+            for (int iRow = 1; iRow <= nRow; iRow++)
+            {
+                for (int iCol = 1; iCol <= nCol; iCol++)
+                {
+                    iCount++;
+                    resRange.Cells[iCount, 1] = actRange.Cells[iRow, iCol];
+
+                }
+            }
+            resRange.Select();
+        }
+        private void btnToRowByCol_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range actRange = Globals.ThisAddIn.Application.ActiveWindow.RangeSelection;
+
+            actRange = GetDataRange("已选择范围：", actRange.Address.ToString());
+
+            if (actRange == null)
+            {
+                return;
+            }
+
+            Excel.Range resRange;
+
+            resRange = GetDataRange("选择结果地址", System.Type.Missing);
+
+            if (resRange == null)
+            {
+                return;
+            }
+            int nRow = actRange.Rows.Count;
+            int nCol = actRange.Columns.Count;
+            int iCount = 0;
+            for (int iCol = 1; iCol <= nCol; iCol++)
+            {
+                for (int iRow = 1; iRow <= nRow; iRow++)
+                {
+                    iCount++;
+                    resRange.Cells[1, iCount] = actRange.Cells[iRow, iCol];
+
+                }
+            }
+            resRange.Select();
+        }
+
+        private void btnToRowByRow_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range actRange = Globals.ThisAddIn.Application.ActiveWindow.RangeSelection;
+
+            actRange = GetDataRange("已选择范围：", actRange.Address.ToString());
+
+            if (actRange == null)
+            {
+                return;
+            }
+
+            Excel.Range resRange;
+
+            resRange = GetDataRange("选择结果地址", System.Type.Missing);
+
+            if (resRange == null)
+            {
+                return;
+            }
+
+            int nRow = actRange.Rows.Count;
+            int nCol = actRange.Columns.Count;
+            int iCount = 0;
+            for (int iRow = 1; iRow <= nRow; iRow++)
+            {
+                for (int iCol = 1; iCol <= nCol; iCol++)
+                {
+                    iCount++;
+                    resRange.Cells[1, iCount] = actRange.Cells[iRow, iCol];
+
+                }
+            }
+            resRange.Select();
+        }
+    }
+}
---
+++ TBATools/ThisAddIn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace TBATools
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+        public Excel.Worksheet GetActiveWorkSheet()
+        {
+            return (Excel.Worksheet)Application.ActiveSheet;
+        }
+
+        public double HeronicCal(int a,int b, int c)
+        {
+            double S = (a + b + c) / 2;
+            double area = Math.Sqrt(S * (S - a) * (S - b) * (S - c));
+            return area;
+        }
+
+        #region VSTO 生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
